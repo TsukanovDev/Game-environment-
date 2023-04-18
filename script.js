1172c59543cc789501f100f9d44de101b2848a80
Viktor <tsukanov87@mail.ru>
2023-04-18 11:56:49 +0300

    Player character

--- script.js
+++ script.js
@@ -14,10 +14,12 @@ window.addEventListener('load', function () {
 			this.game = game;
 			window.addEventListener('keydown', e => {
 				this.game.lastkey = 'P' + e.key;
+				console.log(this.game.lastkey);
 
 			});
 			window.addEventListener('keyup', e => {
 				this.game.lastkey = 'R' + e.key;
+				console.log(this.game.lastkey);
 
 			});
 		}
@@ -25,9 +27,66 @@ window.addEventListener('load', function () {
 
 
 	class Owlbear {
+		constructor(game) {
+			this.game = game;
+			this.width = 100;
+			this.height = 100;
+			this.x = 200;
+			this.y = 200;
+			this.speedX = 0;
+			this.speedY = 0;
+			this.maxSpeed = 10;
+		}
+		draw(context) {
+			context.fillRect(this.x, this.y, this.width, this.height);
+		}
+
+		setSpeed(speedX, speedY) {
+			this.speedX = speedX;
+			this.speedY = speedY;
+		}
+
+		update() {
+			if (this.game.lastkey == 'PArrowLeft') {
+				this.setSpeed(-this.maxSpeed, 0);
 
 
+			} else if (this.game.lastkey == 'PArrowRight') {
+				this.setSpeed(this.maxSpeed, 0);
+
+
+			} else if (this.game.lastkey == 'PArrowUp') {
+				this.setSpeed(0, -this.maxSpeed);
+
+			} else if (this.game.lastkey == 'PArrowDown') {
+				this.setSpeed(0, this.maxSpeed);
+
+
+
+			} else {
+				this.setSpeed(0, 0);
+			}
+			this.x += this.speedX;
+			this.y += this.speedY;
+			//horizontalboundares
+
+			if (this.x < 0) {
+				this.x = 0;
+			} else if (this.x > this.game.width - this.width) {
+				this.x = this.game.width - this.width;
+			}
+
+			//vertical boundares
+
+			if (this.y < 0 + this.game.topMargin) {
+				this.y = 0 + this.game.topMargin;
+			} else if (this.y > this.game.height - this.height) {
+				this.y = this.game.height - this.height
+			}
+		}
 	}
+
+
 
 
 	class Object {
@@ -39,13 +98,28 @@ window.addEventListener('load', function () {
 		constructor(width, height) {
 			this.width = width;
 			this.height = height;
+
 			this.lastkey = undefined;
 			this.input = new InputHandler(this);
+			this.Owlbear = new Owlbear(this);
+			this.topMargin = 200;
+		}
+		render(context) {
+			this.Owlbear.draw(context);
+			this.Owlbear.update();
 		}
 	}
 
 
 	const game = new Game(canvas.width, canvas.height);
-	console.log(game);
+	game.render(ctx);
+
+	function animate() {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		game.render(ctx);
+		requestAnimationFrame(animate);
+	}
+	animate();
+
 });
 
